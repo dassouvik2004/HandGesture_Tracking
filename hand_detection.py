631a29b1ef6312e397e770e87acf00b535563295
dassouvik2004 <souvikdas9102004@gmail.com>
2026-03-13 13:49:50 +0530

    Clean commit without venv

---
+++ hand_detection.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgb)
+
+    if result.multi_hand_landmarks:
+        print("Raja Khanki")
+
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
